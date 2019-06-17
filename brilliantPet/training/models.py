7469ad4611260b5f95869f5b1aa7d3434b7d2786
brilliantPet <alan@brilliantpet.com>
2019-06-17 13:27:20 +0000

    pre pull

--- brilliantPet/training/models.py
+++ brilliantPet/training/models.py
@@ -10,8 +10,4 @@ class Training(models.Model):
     link = models.CharField(max_length=145, null=False)
     type = models.CharField(max_length=45, null=False)
     is_active = models.IntegerField(null=False, default = 1)
-<<<<<<< HEAD
     created_date = models.DateField(auto_now_add=True)
-=======
-    created_date = models.DateField(auto_now_add=True)
->>>>>>> 595b93d5f57d559438d4ff6e62dda1269dce7af5
